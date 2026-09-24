5b1f651d711b7ff7aeec5b4426d2f2a70a2c08b3
idccris <chris_0305@live.com>
2026-09-24 15:54:17 -0700

    Ativa Vercel Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope, Sora } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import { CartProvider } from "../components/cart-provider";
 import { jsonLd, organizationSchema, siteUrl } from "../lib/seo";
 
@@ -39,11 +40,11 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <link rel="stylesheet" href="/tiger-global.css?v=20260912-2" />
-        <link rel="stylesheet" href="/catalog-download.css?v=20260917" />
       </head>
       <body className={`${manrope.variable} ${sora.variable}`}>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: jsonLd(organizationSchema) }} />
         <CartProvider>{children}</CartProvider>
+        <Analytics />
       </body>
     </html>
   );
